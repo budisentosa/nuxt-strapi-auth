737c6219677fd38f003f274b276b6e689ed6ea0c
Dicky Jiang <dickyjuwono@gmail.com>
2021-09-28 13:38:36 +0700

    1st

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c21689c81e769ed52518a99666a7eb0f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ae174e8e-ed5a-46aa-a174-1c21f11530c4'
+};
